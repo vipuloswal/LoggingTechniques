79e378c8072ad475078061a883cc21c8fdc8afe5
vipuloswal <vipul.oswal2@gmail.com>
2023-04-09 11:55:44 +1000

    Updated logger file configuration

--- src/test/java/LoggerOptions.java
+++ src/test/java/LoggerOptions.java
@@ -1,6 +1,6 @@
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //import org.apache.logging.log4j.LogManager;
 //import org.apache.logging.log4j.Logger;
@@ -12,20 +12,20 @@ public class LoggerOptions {
 
     public static void main(String args[]) {
 
-        /*//Logging using default java provided logger
-        java.util.logging.Logger logger= java.util.logging.Logger.getLogger(LoggerOptions.class.getName());
-        logger.info("Logging using default java logging");*/
+        //Logging using default java provided logger
+        java.util.logging.Logger defaultLogger= java.util.logging.Logger.getLogger(LoggerOptions.class.getName());
+        defaultLogger.info("Logging using default java logging");
 
-        /*//Logging using log4j 2. logger
-        Logger logger = LogManager.getLogger(LoggerOptions.class);
-        logger.error("Logging using log4j2.17.1 java logging");*/
+      /*  //Logging using log4j 2. logger
+        Logger log4jLogger = LogManager.getLogger(LoggerOptions.class);
+        log4jLogger.error("Logging using log4j2.17.1 java logging");*/
 
-        /*//Logging using slf4j logger
-        Logger logger = LoggerFactory.getLogger(LoggerOptions.class);
-        logger.info("Logging using slf4j logging with slf4j simple implementation");*/
+        //Logging using slf4j logger
+        Logger slf4jLogger = LoggerFactory.getLogger(LoggerOptions.class);
+        slf4jLogger.info("Logging using slf4j logging with slf4j simple implementation");
 
-        /*//Logging using Logback
-        Logger logger = LoggerFactory.getLogger(LoggerOptions.class);
-        logger.info("Logging using slf4j logging with logback");*/
+        //Logging using Logback
+        Logger logbackLogger = LoggerFactory.getLogger(LoggerOptions.class);
+        logbackLogger.info("Logging using slf4j logging with logback");
     }
 }
